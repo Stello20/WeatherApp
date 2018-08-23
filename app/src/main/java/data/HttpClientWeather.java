2d2c33f322af991d90a164a260b362402bfb7556
Stello20 <castellog20@gmail.com>
2018-08-23 11:57:48 -0100

    Working on retrieving location from device and JSON file from weather API

--- app/src/main/java/data/HttpClientWeather.java
+++ app/src/main/java/data/HttpClientWeather.java
@@ -1,7 +1,13 @@
 package data;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpClientWeather {
@@ -10,12 +16,48 @@ public class HttpClientWeather {
     public static final String API_URL_Forcast5 = "api.openweathermap.org/data/2.5/forecast?lat=";
     public static final String API_URL_Weather = "api.openweathermap.org/data/2.5/weather?lat=";
 
-    public String getJSONWeatherData(int lat, int lon) {
+    public static String getJSONWeatherData(float lat, float lon) {
         HttpURLConnection connection = null;
-        InputStream inputStream = null;
+        BufferedReader reader = null;
 
-        try{
-            connection =(HttpURLConnection)(new URL(API_URL_Weather+lat+"&lon="+lon)).openConnection();
+        try {
+            URL url = new URL(API_URL_Forcast5+lat+"&lon="+lon);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line+"\n");
+                Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+            }
+
+            return buffer.toString();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return null;
     }
 }
--- app/src/main/java/za/co/inventwithcode/android/weatherapp/MainActivity.java
+++ app/src/main/java/za/co/inventwithcode/android/weatherapp/MainActivity.java
@@ -9,21 +9,38 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.widget.TextView;
 import android.widget.Toast;
+import data.HttpClientWeather;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Console;
+
+import data.HttpClientWeather;
+
 public class MainActivity extends AppCompatActivity {
 
-
-
+    TextView d1Temperature, d2Temperature, d3Temperature, d4Temperature, d5Temperature;
+    TextView d1Day, d2Day, d3Day, d4Day, d5Day;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        d1Temperature = (TextView) findViewById(R.id.d1Temperature);
+        d2Temperature = (TextView) findViewById(R.id.d2Temperature);
+        d3Temperature = (TextView) findViewById(R.id.d3Temperature);
+        d4Temperature = (TextView) findViewById(R.id.d4Temperature);
+        d5Temperature = (TextView) findViewById(R.id.d5Temperature);
+
+        d2Day.setText("Tuesday");
+
 
         // Acquire a reference to the system Location Manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -32,30 +49,39 @@ public class MainActivity extends AppCompatActivity {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                RefreshWeather(location);
+                //Refresh5DaysWeather(location);
+                Toast.makeText(MainActivity.this, location.getLatitude()+"", Toast.LENGTH_SHORT).show();
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
+                Toast.makeText(MainActivity.this, "Status Changed", Toast.LENGTH_SHORT).show();
             }
 
             public void onProviderEnabled(String provider) {
+                Toast.makeText(MainActivity.this, "Provider Enabled", Toast.LENGTH_SHORT).show();
             }
 
             public void onProviderDisabled(String provider) {
+                Toast.makeText(MainActivity.this, "Provider Disabled", Toast.LENGTH_SHORT).show();
             }
+
         };
+
 
 // Register the listener with the Location Manager to receive location updates
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Location Access required to update weather forcasts", Toast.LENGTH_LONG);
         }
         else{
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, locationListener);
+            Toast.makeText(this, "Location requested", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void RefreshWeather(Location loc){
-
+    public void Refresh5DaysWeather(Location loc){
+        d1Temperature.setText("21");
     }
+
+    //Log.d("JSONData>>",HttpClientWeather.getJSONWeatherData(23, 23));
 
 }
